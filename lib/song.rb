6569b72bb8a6c89354f4c827e8292d84aadf45ea
Devin Wilson <craigvin84@yahoo.com>
2020-11-20 00:10:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 class Song
   include Paramble
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
